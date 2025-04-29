805880a8bf9eefc9579d19de4f9c8d2d649cb704
Memphis <kvn_araneda@hotmail.com>
2025-04-29 00:00:40 -0400

    Creo otro objeto

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,16 @@ public class Main {
         System.out.println("Su fecha de lanzamiento es: "+ pelicula1.fechaLanzamiento);
         System.out.println("Su duración es: " + pelicula1.duracionMinutos + " minutos.");
 
+        Peliculas pelicula2 = new Peliculas();
+        pelicula2.nombre = "Matrix";
+        pelicula2.fechaLanzamiento = 1998;
+        pelicula2.duracionMinutos = 180;
+
+        System.out.println("\nMi pelicula es: "+ pelicula2.nombre);
+        System.out.println("Su fecha de lanzamiento es: "+ pelicula2.fechaLanzamiento);
+        System.out.println("Su duración es: " + pelicula2.duracionMinutos + " minutos.");
+
+
 
     }
 }
